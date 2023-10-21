02f54e91e23ca96c3b0f004dda2ce7df04bcec54
mafiageek <anton_kho@msn.com>
2023-10-21 08:43:09 +0800

    added docker file

--- server/src/main/java/ntu/edu/sg/project3/Swagger/SpringFoxConfig.java
+++ server/src/main/java/ntu/edu/sg/project3/Swagger/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package ntu.edu.sg.project3.swagger;
+package ntu.edu.sg.project3.Swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
